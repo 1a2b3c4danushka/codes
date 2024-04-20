e9c88c0b3176404c68e339f107e034a8618fb478
1a2b3c4danushka <anu.shka070203@gmail.com>
2024-04-20 22:02:52 +0530

    commit

---
+++ except.java
@@ -0,0 +1,14 @@
+
+public class except{
+    public static void main(String[]args){
+        int a = 5;
+        int b = 0;
+        try{
+            int c = a/b;
+            System.out.println(c);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
