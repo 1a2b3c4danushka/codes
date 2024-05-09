9b45f5e1fd30b0ecaf8f30ee19c5a3ef9f06e7ea
1a2b3c4danushka <anu.shka070203@gmail.com>
2024-05-09 21:07:33 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class MyThread extends Thread{
+    public void greet(){
+        System.err.println("Hello from my side");
+    }
+
+}
+class MyThread2 extends Thread{
+    public void gee(){
+        System.out.println("Iam Anushka");
+    }
+}
+public class Main{
+    public static void main(String[]args)
+    {
+        MyThread t1 = new MyThread();
+        MyThread2 t2 = new MyThread2();
+        t1.start();
+        t2.start();
+    }
+}
